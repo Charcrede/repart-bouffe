eaf275c8e2ca18af0507371490b7dcdef44b3dd2
junior-explotel <junior_bj@explotel.net>
2025-05-07 15:15:44 +0100

    retrait de Head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from '@/components/ThemeProvider'
-import Head from "next/head";
 
 
 const geistSans = Geist({
